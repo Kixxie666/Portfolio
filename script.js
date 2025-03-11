b7cde95bd7fb6a4c509b34dedf20de43a3d6b3b4
Kixxie666 <kixxie6@outlook.com>
2025-03-11 18:36:19 +0000

    //

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", function () {
     const themeToggle = document.getElementById("theme-toggle");
     const body = document.body;
     
-    // Smooth scrolling
+
     navLinks.forEach(link => {
         link.addEventListener("click", function (event) {
             event.preventDefault();
@@ -17,12 +17,6 @@ document.addEventListener("DOMContentLoaded", function () {
                 });
             }
         });
-    });
-
-    // Theme toggle functionality
-    themeToggle.addEventListener("click", function () {
-        body.classList.toggle("dark-mode");
-        localStorage.setItem("theme", body.classList.contains("dark-mode") ? "dark" : "light");
     });
 
     // Load saved theme preference
